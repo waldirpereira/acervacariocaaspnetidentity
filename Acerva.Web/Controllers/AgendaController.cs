59d4538d7d4fc900f3a82a9b621e594b3b6659b6
waldirpereira <waldirpereira@gmail.com>
2017-02-03 17:34:19 -0200

    adicionando agenda

---
+++ Acerva.Web/Controllers/AgendaController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using Acerva.Infra.Repositorios;
+
+namespace Acerva.Web.Controllers
+{
+    public class AgendaController : ApplicationBaseController
+    {
+        public AgendaController(ICadastroUsuarios cadastroUsuarios) : base(cadastroUsuarios)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

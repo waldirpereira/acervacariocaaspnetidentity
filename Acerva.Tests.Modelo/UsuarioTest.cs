c7683efde1ec92b4dcd8e4b8c687cf601ab68441
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2017-03-17 07:39:52 -0300

    criacao de testes da classe Usuario

---
+++ Acerva.Tests.Modelo/UsuarioTest.cs
@@ -0,0 +1,48 @@
+﻿using Acerva.Modelo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acerva.Tests.Modelo
+{
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void EstaAssociadoDeveriaRetrnarTrueSeStatusEhAtivo()
+        {
+            // given
+            var usuario = new Usuario { Status = StatusUsuario.Ativo };
+
+            // when
+
+
+            // then
+            Assert.IsTrue(usuario.EstaAssociado);
+        }
+
+        [TestMethod]
+        public void EstaAssociadoDeveriaRetrnarTrueSeStatusEhAguardandoRenovacao()
+        {
+            // given
+            var usuario = new Usuario { Status = StatusUsuario.AguardandoRenovacao };
+
+            // when
+
+
+            // then
+            Assert.IsTrue(usuario.EstaAssociado);
+        }
+
+        [TestMethod]
+        public void EstaAssociadoDeveriaRetrnarTrueSeStatusNaoEhAtivoNemAguardandoRenovacao()
+        {
+            // given
+            var usuario = new Usuario { Status = StatusUsuario.AguardandoPagamentoAnuidade };
+
+            // when
+
+
+            // then
+            Assert.IsFalse(usuario.EstaAssociado);
+        }
+    }
+}

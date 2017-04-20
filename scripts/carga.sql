e0ac7f6de21b7f66726d3a0b8133a8304f936f14
waldirpereira <waldirpereira@gmail.com>
2017-04-20 17:54:31 -0300

    atualizacao da carga de historico

--- scripts/carga.sql
+++ scripts/carga.sql
@@ -2,5 +2,5 @@
 
 
 /*CARGA DE HISTORICO DE STATUS*/
-INSERT INTO `historico_status_usuario`(`id_usuario_alterado`, `data_hora`, `status_novo`, `nome_usuario_logado`) 
-select id, now(), status, 'waldirpereira@gmail.com' from users;
+INSERT INTO `historico_status_usuario`(`id_usuario_alterado`, `data_hora`, `status_novo`, `id_usuario`) 
+select id, now(), status, '6be52512-d272-4cc2-98a9-f5bf93534082' from users u where not exists (select 1 from historico_status_usuario h where u.id = h.id_usuario_alterado);

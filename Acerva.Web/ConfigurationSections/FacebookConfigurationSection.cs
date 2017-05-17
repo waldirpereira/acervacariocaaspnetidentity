d2832e6df3f7171e10e8f0529311ec4bac75bf5f
waldirpereira <waldirpereira@gmail.com>
2017-05-17 16:06:13 -0300

    atualizacao do Owin para funcionar o login pelo Facebook

---
+++ Acerva.Web/ConfigurationSections/FacebookConfigurationSection.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+
+namespace Acerva.Web.ConfigurationSections
+{
+    public class FacebookConfigurationSection : ConfigurationSection
+    {
+        [ConfigurationProperty("appId")]
+        public string AppId
+        {
+            get { return (string)base["appId"]; }
+            set { base["appId"] = value; }
+        }
+
+        [ConfigurationProperty("appSecret")]
+        public string AppSecret
+        {
+            get { return (string)base["appSecret"]; }
+            set { base["appSecret"] = value; }
+        }
+
+        public FacebookConfigurationSection()
+        {
+        }
+
+    }
+}

23f54573e61338e44d38241892854c7ffaef58af
waldirpereira <waldirpereira@gmail.com>
2018-01-03 16:25:45 -0200

    Correção de bug ao utilizar o LojaController enquanto logado

--- Acerva.Web/Controllers/LojaController.cs
+++ Acerva.Web/Controllers/LojaController.cs
@@ -1,9 +1,14 @@
 ﻿using System.Web.Mvc;
+using Acerva.Infra.Repositorios;
 
 namespace Acerva.Web.Controllers
 {
-    public class LojaController : Controller
+    public class LojaController : ApplicationBaseController
     {
+        public LojaController(ICadastroUsuarios cadastroUsuarios) : base(cadastroUsuarios)
+        {
+        }
+
         public ActionResult Index()
         {
             return View();
